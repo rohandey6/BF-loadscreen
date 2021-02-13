550d8b3a438b6cdf4c1429ba8780cf889ade2e32
NIGHTMARE <58006093+rohandey6@users.noreply.github.com>
2021-02-13 11:54:30 +0530

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,28 @@
+files {
+    'index.html',
+    'style.css',
+    'images/*',
+    'script.js',
+    'vue.min.js'
+}
+
+loadscreen 'index.html'
+
+loadscreen_manual_shutdown "yes"
+
+client_script "client.lua"
+
+fx_version 'cerulean'
+games {'gta5'}
+
+
+---Rare5m.co
+---https://discord.gg/pNpjRqesgp
+--https://discord.gg/bhucJRBxRz
+--https://discord.gg/szZmSGZgwp
+---
+---
+--
+--
+--
+--
